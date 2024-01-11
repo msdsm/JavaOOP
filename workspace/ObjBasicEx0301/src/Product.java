bfaff9a8cf8b44d8754e3960ae329070f2c63fce
msdsm <masuda38836050@gmail.com>
2024-01-11 21:13:29 +0900

    finish 3

---
+++ workspace/ObjBasicEx0301/src/Product.java
@@ -0,0 +1,20 @@
+
+public class Product {
+	String name;
+	int price;
+	
+	int up(int amt) {
+		price -= amt;
+		return price;
+	}
+	
+	int down(int amt)
+	{
+		price -= amt;
+		return price;
+	}	
+	
+	void display() {
+		System.out.println(name + ":" + price);
+	}
+}
