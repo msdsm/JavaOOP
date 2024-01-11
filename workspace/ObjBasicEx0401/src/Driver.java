304f8e0e82729484121f6f37540daad50e7ec922
msdsm <masuda38836050@gmail.com>
2024-01-11 21:38:51 +0900

    finish 4

---
+++ workspace/ObjBasicEx0401/src/Driver.java
@@ -0,0 +1,15 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Product prod = new Product();
+		
+		prod.name = "キーボード";
+		prod.price = 2000;
+		prod.display();
+		prod.down(400);
+		prod.display();
+	}
+
+}
